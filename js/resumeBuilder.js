bfb83f9fd0ebadf57db63193a24ff7c97e02d01f
Rena Goldsmith <rena.kg@gmail.com>
2016-04-27 14:32:45 +1200

    add function

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,21 +1,29 @@
-var quote = {
+var randomQuote = {
   "quotes": [
     {
-      "quote": "If you haven't got anything nice to say about anybody, come sit next to me."
-      "author": "Alice Roosevelt Longworth",
+      "quote": "If you haven't got anything nice to say about anybody, come sit next to me.",
+      "author": "Alice Roosevelt Longworth"
     },
     {
-      "quote": "Do not take life too seriously. You will never get out of it alive."
+      "quote": "Do not take life too seriously. You will never get out of it alive.",
       "author": "Elbert Hubbard"
     },
     {
-      "quote": "Wassuuuuuuuuup"
-      "author": "Scary Movie 1",
+      "quote": "Wassuuuuuuuuup",
+      "author": "Scary Movie 1"
     }
   ],
 
 };
 
+function displayQuote(){
+  for (quote in randomQuote.quotes) {
+    $('#quote').append(HTMLquote);
+    var formattedQuote = HTMLquoteContent.replace('%data%', randomQuote.quotes[quote].quote);
+    $('.quote-entry').append(formattedQuote);
+  }
+};
+displayQuote();
 
 $('#quote').append(quoteButton);
 
