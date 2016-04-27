8060522d2a69c2d1b27db86ac85f91bbe4ddd05c
Kristy <johnstonekristy@gmail.com>
2016-04-27 17:25:11 +1200

    add quotes

--- js/quoteBuilder.js
+++ js/quoteBuilder.js
@@ -5,12 +5,40 @@ var randomQuote = {
       "author": "Alice Roosevelt Longworth"
     },
     {
-      "quote": "Wassuuuuuuuuup",
-      "author": "Scary Movie 1"
-    },
-    {
       "quote": "Do not take life too seriously. You will never get out of it alive.",
       "author": "Elbert Hubbard"
+    },
+    {
+      "quote": "In three words I can sum up everything I've learned about life: it goes on.",
+      "author": "Robert Frost"  
+    },
+    {
+      "quote": "Live as if you were to die tomorrow. Learn as if you were to live forever.",
+      "author": "Mahatma Gandhi"
+    },
+    {
+      "quote": "If you tell the truth, you don't have to remember anything.",
+      "author": "Mark Twain"
+    },
+    {
+      "quote": "A friend is someone who knows all about you and still loves you.",
+      "author": "Elbert Hubbard"
+    },
+    {
+      "quote": "To live is the rarest thing in the world. Most people exist, that is all.",
+      "author": "Oscar Wilde"
+    },
+    {
+      "quote": "Even if you’re on the right track, you’ll get run over if you just sit there.",
+      "author": "Will Rogers"
+    },
+    {
+      "quote": "In the presence of nature, a wild delight runs through the man, in spite of real sorrows.",
+      "author": "Ralph Waldo Emerson"
+    },
+    {
+      "quote": "The earth has music for those who listen.",
+      "author": "William Shakespeare"
     }
   ],
 };
@@ -24,8 +52,6 @@ function displayQuote(){
     quoteArray.push(grabQuote);
   }
     return quoteArray;
-
-
 };
 displayQuote();
 
@@ -33,12 +59,11 @@ var refreshQuote = function() {
   window.location.reload();
 }
 
-
-
-
 $('#quote').append(displayQuote)
 
 
-
+$(".button").on("click", function() {
+  displayQuote();
+});
 
 
